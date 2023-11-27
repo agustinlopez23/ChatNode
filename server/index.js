e0163dc920b7898a521a5cd66bf7b0a31733cba2
Your Name <you@example.com>
2023-11-26 22:50:10 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import logger from 'morgan'
+
+const port = process.env.PORT ?? 300
+
+const app = express()
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+  res.send('<h1>Esto es el chat</h1>')
+})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
